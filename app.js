72fe46c5ab290580f29005d8b32a99c3fd646d06
Youngjae Kim <yj122198@naver.com>
2021-02-01 00:39:33 +0900

    api login

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+const { PythonShell } = require("python-shell");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  var html = `
+    <form action="/login" method="post">
+        <p><input type ="text" name ="id" placeholder="학번"/></p>
+        <p><input type ="password" name ="pw" placeholder="비밀번호"/></p>
+        <p><input type="submit" value="로그인"/></p>
+    </form>
+  `;
+  res.send(html);
+});
+
+app.post("/login", (req, res) => {
+  var options = {
+    mode: "text",
+    pythonPath: "",
+    pythonOptions: ["-u"],
+    scriptPath: "",
+    args: [req.body.id, req.body.pw]
+  };
+  PythonShell.run("test.py", options, (err, results) => {
+    if (err) throw err;
+
+    console.log("results : ", results);
+    res.send(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("connection!");
+});
