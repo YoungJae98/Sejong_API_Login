30e6b12f8202acdb76f7942c3ff5e889d3da94a0
Youngjae Kim <yj122198@naver.com>
2021-07-31 16:54:31 +0900

    python to nodejs with axios

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var request = require("request");
 var axios = require("axios");
-var convert = require("xml-js");
-const { PythonShell } = require("python-shell");
-const { post } = require("request");
+var tough = require("tough-cookie");
+var FormData = require("form-data");
+var axiosCookieJarSupport =
+  require("@3846masa/axios-cookiejar-support").default;
+
+var instance = axios.create({ withCredentials: true });
+axiosCookieJarSupport(instance);
+instance.defaults.jar = new tough.CookieJar();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   var html = `
-    <form action="https://do.sejong.ac.kr/ko/process/member/login" method="post">
+    <form action="/login" method="post">
         <p><input type ="text" name ="email" placeholder="학번"/></p>
         <p><input type ="password" name ="password" placeholder="비밀번호"/></p>
         <p><input type="submit" value="로그인"/></p>
@@ -24,37 +28,47 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
-  axios
-    .post("https://do.sejong.ac.kr/ko/process/member/login", {
-      email: email,
-      password: password
+  var bodyFormData = new FormData();
+
+  bodyFormData.append("email", email);
+  bodyFormData.append("password", password);
+
+  instance
+    .post("https://do.sejong.ac.kr/ko/process/member/login", bodyFormData, {
+      withCredentials: true,
+      headers: bodyFormData.getHeaders()
     })
     .then((response) => {
-      console.log(response);
-      res.send("hello");
+      instance.get("https://do.sejong.ac.kr/").then((response) => {
+        console.log(response);
+        res.send("hello");
+      });
     });
-
-  //   request.post(
-  //     {
-  //       uri: "https://do.sejong.ac.kr/ko/process/member/login",
-  //       form: dataObj,
-  //       json: true
+  // request.post(
+  //   {
+  //     uri: "https://do.sejong.ac.kr/ko/process/member/login",
+  //     form: {
+  //       email: email,
+  //       password: password
   //     },
-  //     (err, sessionResponse, body) => {
-  //       var cookie = sessionResponse.caseless["dict"]["set-cookie"];
-  //       request.get(
-  //         {
-  //           uri: "https://do.sejong.ac.kr/ko/process/member/login",
-  //           headers: {
-  //             Cookie: cookie
-  //           }
-  //         },
-  //         (err, response, body) => {
-  //           res.send(response.body);
-  //         }
-  //       );
-  //     }
-  //   );
+  //     json: true
+  //   },
+  //   (err, sessionResponse, body) => {
+  //     var cookie = sessionResponse.caseless["dict"]["set-cookie"];
+  //     res.send(body);
+  //     // request.get(
+  //     //   {
+  //     //     uri: "https://do.sejong.ac.kr/ko/process/member/login",
+  //     //     headers: {
+  //     //       Cookie: cookie
+  //     //     }
+  //     //   },
+  //     //   (err, response, body) => {
+  //     //     res.send(response.body);
+  //     //   }
+  //     // );
+  //   }
+  // );
 });
 
 app.listen(3000, () => {
